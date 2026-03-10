9d698be4dbf1978bb5043ead1491c7ad3c457ec4
yokino-dev <lannyke7@gmail.com>
2026-03-10 12:10:40 -0300

    Testando Imagens

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from docx import Document
-from docx.enum.table import WD_TABLE_ALIGNMENT
+from docx.shared import Inches
 
 document = Document()
 
 
 document.add_heading("Titulo Centralizado", 0)
-p = document.add_paragraph("Teste de Paragrafo: 1    ")
+p = document.add_paragraph("Teste de Paragrafo: 1")
 
 p.add_run("Teste de Paragrafo: 2").italic = True
-
+document.add_picture("goku.png", width=Inches(1.25))
 
 document.save("neo.docx")
