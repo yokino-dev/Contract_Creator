a6e73468676666cb1002adcfd0aba76217b1fe76
yokino-dev <lannyke7@gmail.com>
2026-03-10 12:00:43 -0300

    Teste do Italico e paragrafo

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from docx import Document
 from docx.enum.table import WD_TABLE_ALIGNMENT
 
 document = Document()
-header = document.add_table(3,3)
-header.alignment = WD_TABLE_ALIGNMENT
 
-document.add_heading("Teste 1: Título Centralizado", 0)
+
+document.add_heading("Titulo Centralizado", 0)
+p = document.add_paragraph("Teste de Paragrafo: 1    ")
+
+p.add_run("Teste de Paragrafo: 2").italic = True
 
 
 document.save("neo.docx")
