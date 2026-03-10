6a2341fdb1d64aac61871d66f1ef95124c5073e5
yokino-dev <lannyke7@gmail.com>
2026-03-10 11:49:42 -0300

    Primeiro Teste de Alinhamento de titulo

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from docx import Document
+from docx.enum.table import WD_TABLE_ALIGNMENT
 
 document = Document()
+header = document.add_table(3,3)
+header.alignment = WD_TABLE_ALIGNMENT
 
-document.add_heading("Titulo Marinho da silva", 0)
+document.add_heading("Teste 1: Título Centralizado", 0)
 
 
 document.save("neo.docx")
